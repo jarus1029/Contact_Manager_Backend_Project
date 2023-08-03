49832d5088eac399e90ecdf016611a7ad454927f
Suraj <surajkumar1029ss@gmail.com>
2023-08-04 00:17:22 +0530

    first commit

---
+++ backend/routes/contactRoutes.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const {getContacts,createContact,updateContact,getContact,deleteContact}=require("../controllers/contactController")
+const validateToken=require("../middleware/validateTokenHandler")
+const router=express.Router();
+router.use(validateToken);
+
+router.route('/').get(getContacts);
+
+router.route('/').post(createContact);
+
+router.route('/:id').put(updateContact);
+
+router.route('/:id').get(getContact);
+
+router.route('/:id').delete(deleteContact);
+
+module.exports=router;
